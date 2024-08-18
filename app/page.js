0606e5f78574fb01d4046d8bdb66456aff770228
ahmed <ahmedboabae2@gmail.com>
2024-08-18 14:09:28 +0000

    hero section redesign

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 "use client";
 import getStripe from "../utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Box, Button, Grid, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Grid,
+  Toolbar,
+  Typography,
+  Card,
+  CardContent,
+  Container,
+} from "@mui/material";
+import Image from "next/image"; // Import Image component from Next.js
 
 const handleSubmit = async (subscriptionType) => {
   const checkoutSession = await fetch("/api/checkout_sessions", {
@@ -24,10 +35,10 @@ const handleSubmit = async (subscriptionType) => {
 export default function Home() {
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
         <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Flashcard SaaS
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            StudyMate
           </Typography>
           <SignedOut>
             <Button color="inherit" href="/sign-in">
@@ -43,118 +54,172 @@ export default function Home() {
         </Toolbar>
       </AppBar>
 
-      <Box sx={{ textAlign: "center", my: 4 }}>
-        <Typography variant="h2" component="h1" gutterBottom>
-          Welcome to Study Mate
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          The easiest way to create summary notes.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ mt: 2, mr: 2 }}
-          href="/generate"
-        >
-          Get Started
-        </Button>
-        <Button variant="outlined" color="primary" sx={{ mt: 2 }}>
-          Learn More
-        </Button>
-      </Box>
+      <Container maxWidth="md" sx={{ my: 20 }}>
+        <Grid container spacing={4} alignItems="center">
+          {/* Image Section */}
+          <Grid item xs={12} md={6}>
+            <Image
+              src="/images/study.jpg" // Replace with the path to your image
+              alt="Someone studying"
+              width={400}
+              height={250}
+              style={{ borderRadius: "8px" }}
+            />
+          </Grid>
 
-      <Box sx={{ my: 6 }}>
-        <Typography variant="h4" component="h2" gutterBottom>
-          Features
-        </Typography>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6">Lorem ipsum</Typography>
-            <Typography>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Habitant
-              ipsum dictumst odio amet donec augue sagittis ridiculus.
+          {/* Text and Buttons Section */}
+          <Grid item xs={12} md={6}>
+            <Typography
+              variant="h2"
+              component="h1"
+              gutterBottom
+              sx={{ fontWeight: "bold", color: "#3f51b5" }}
+            >
+              Welcome to Study Mate
             </Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6">Lorem ipsum</Typography>
-            <Typography>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Habitant
-              ipsum dictumst odio amet donec augue sagittis ridiculus.
+            <Typography
+              variant="h5"
+              component="h2"
+              gutterBottom
+              sx={{ color: "text.secondary", mb: 4 }}
+            >
+              The easiest way to create summary notes.
             </Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Typography variant="h6">Lorem ipsum</Typography>
-            <Typography>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Habitant
-              ipsum dictumst odio amet donec augue sagittis ridiculus.
-            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2, mr: 2, px: 4, py: 1.5 }}
+              href="/generate"
+            >
+              Get Started
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{ mt: 2, px: 4, py: 1.5 }}
+            >
+              Learn More
+            </Button>
           </Grid>
         </Grid>
-      </Box>
 
-      <Box sx={{ my: 6, textAlign: "center" }}>
-        <Typography variant="h4" component="h2" gutterBottom>
-          Pricing
-        </Typography>
-        <Grid container spacing={4} justifyContent="center">
-          <Grid item xs={12} md={4}>
-            <Box
-              sx={{
-                p: 3,
-                border: "1px solid",
-                borderColor: "grey.300",
-                borderRadius: 2,
-              }}
-            >
-              <Typography variant="h5" gutterBottom>
-                Basic
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                $2 / month
-              </Typography>
-              <Typography>
-                Lorem ipsum odor amet, consectetuer adipiscing elit.
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ mt: 2 }}
-                onClick={() => handleSubmit("basic")}
-              >
-                Choose Basic
-              </Button>
-            </Box>
+        <Box sx={{ my: 20, px: 6 }}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            sx={{ fontWeight: "bold", color: "#3f51b5", textAlign: "center" }}
+          >
+            Features
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <Card sx={{ p: 2, height: "100%" }}>
+                <CardContent>
+                  <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                    Easy Text Input
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary", mt: 1 }}>
+                    Simply paste your text whether it's just just one word or
+                    your full notes and let our tool do all the work. It's that
+                    easy
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card sx={{ p: 2, height: "100%" }}>
+                <CardContent>
+                  <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                    AI Powered Summaries
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary", mt: 1 }}>
+                    Our AI algorithm understands your text and generates
+                    relevant study points and solutions giving you the right
+                    direction to study
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card sx={{ p: 2, height: "100%" }}>
+                <CardContent>
+                  <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                    Cloud Storage
+                  </Typography>
+                  <Typography sx={{ color: "text.secondary", mt: 1 }}>
+                    Access your study cards from any device anywhere. Your study
+                    summaries are always saved for easy access
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={4}>
-            <Box
-              sx={{
-                p: 3,
-                border: "1px solid",
-                borderColor: "grey.300",
-                borderRadius: 2,
-              }}
-            >
-              <Typography variant="h5" gutterBottom>
-                Pro
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                $5 / month
-              </Typography>
-              <Typography>
-                Lorem ipsum odor amet, consectetuer adipiscing elit.
-              </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ mt: 2 }}
-                onClick={() => handleSubmit("pro")}
-              >
-                Choose Pro
-              </Button>
-            </Box>
+        </Box>
+
+        <Box sx={{ my: 20, textAlign: "center" }}>
+          <Typography
+            variant="h4"
+            component="h2"
+            gutterBottom
+            sx={{ fontWeight: "bold", color: "#3f51b5" }}
+          >
+            Pricing
+          </Typography>
+          <Grid container spacing={4} justifyContent="center">
+            <Grid item xs={12} md={4}>
+              <Card sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  sx={{ fontWeight: "bold", color: "#3f51b5" }}
+                >
+                  Basic
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                  $2 / month
+                </Typography>
+                <Typography sx={{ color: "text.secondary", mt: 2 }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ mt: 4, px: 4, py: 1.5 }}
+                  onClick={() => handleSubmit("basic")}
+                >
+                  Choose Basic
+                </Button>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card sx={{ p: 3, borderRadius: 2, boxShadow: 3 }}>
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  sx={{ fontWeight: "bold", color: "#3f51b5" }}
+                >
+                  Pro
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                  $5 / month
+                </Typography>
+                <Typography sx={{ color: "text.secondary", mt: 2 }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ mt: 4, px: 4, py: 1.5 }}
+                  onClick={() => handleSubmit("pro")}
+                >
+                  Choose Pro
+                </Button>
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </Container>
     </>
   );
 }
